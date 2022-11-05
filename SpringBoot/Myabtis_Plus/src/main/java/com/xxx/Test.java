d9aff2d1a794e0573d5779343e5dd3f9d5d95410
hepeng <2592620862@qq.com>
2022-11-05 22:05:30 +0800

    用于测试git的提交

---
+++ SpringBoot/Myabtis_Plus/src/main/java/com/xxx/Test.java
@@ -0,0 +1,11 @@
+package com.xxx;
+
+/**
+ * @author HePeng
+ * .@date  2022/11/5 22:04
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("用于测试git提交");
+    }
+}
